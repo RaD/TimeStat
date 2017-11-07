ba645c5dd16a27708302ad237244ef1060c67b18
Ruslan Popov <ruslan.popov@gmail.com>
2017-11-07 21:51:26 +0300

    Integration of CoreData.

---
+++ README.md
@@ -0,0 +1,1 @@
+Шикарная дока по интеграции Core Data: https://cocoacasts.com/setting-up-the-core-data-stack-from-scratch/
--- TimeStat/AppDelegate.swift
+++ TimeStat/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreData
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,7 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        let coreDataManager = CoreDataManager(modelName: "TimeStat")
+        print(coreDataManager.managedObjectContext)
         return true
     }
 
@@ -41,6 +43,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
-
+    // MARK: - Core Data stack    
 }
 
